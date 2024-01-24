644211d385040cd998771dc07f1210085c8e1dde
MarceloRodriguesOliveira <marcelo--rodrigues2@hotmail.com>
2024-01-23 22:01:39 -0300

    visual inconsistency fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full  sm:h-[80vh] flex flex-col sm:flex-col items-center m-auto sm:m-0 border  md:flex-row md:h-4/6">
+    <div className="w-full  sm:h-[80vh] flex flex-col sm:flex-col items-center m-auto sm:m-0 md:flex-row md:h-4/6">
       <div className="w-96  flex flex-col justify-center items-center p-6 sm:mt-6 min-w-[350px] sm:w-3/6   ">
         <h1 className="text-3xl font-bold mb-4 ">Comece Agora!</h1>
         <button className="rounded w-28 p-2 bg-violet-400 text-white font-medium text-xl">
